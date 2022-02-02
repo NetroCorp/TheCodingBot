9c4f0464128f81f69ee5ee04e0c7adffd28d8d7a
TheCodingGuy <officialtcgmatt@gmail.com>
2022-02-01 20:31:08 -0500

    Add messageDeleteBulk for #15
    This completes the message part of events.

--- src/app/evts/messageDelete.js
+++ src/app/evts/messageDelete.js
@@ -8,61 +8,22 @@ module.exports = async(app, message) => {
     //
 
     if (message.guild == null ||
-        message.author.bot) return; // Stop if we in a guild - and stop if we getting data from a bot.
+        message.author.bot) return; // Stop if we not in a guild - and stop if we getting data from a bot.
 
     var serverSettings = await app.DBs.serverSettings.findOne({ where: { serverID: message.guild.id } });
     if (!serverSettings) return;
 
-    if (app.client.waitingForMessageToDelete == undefined) app.client.waitingForMessageToDelete = [];
-
-    async function downloadAttachments(attachments) {
-        var result = { failed: [], succeed: [] };
-        app.client.waitingForMessageToDelete.push(message.id);
-
-        for (var attachment of attachments) {
-            var attachment = attachment[1]; // For some reason, doing it this way, we go into an array? [0] = id, [1] = MessageAttachment
-            var msgAttachURL = ((attachment.proxyURL == null || attachment.proxyURL == "") ? attachment.url : attachment.proxyURL),
-                file = attachment.name;
-            var fileext = file.slice((file.lastIndexOf(".") - 1 >>> 0) + 2);
-            var filename = file.replace("." + fileext, "");
-            // someone tell me if there is a better write to write this.
-
-            var postURL = `${app.config.system.logURL}TCB_Post.php?rand=${app.functions.getTicks()}&type=logging&url=${msgAttachURL}&guildID=${message.guild.id}&channelID=${message.channel.id}&messageID=${message.id}&filename=${filename}&fileext=${fileext}&size=${attachment.size}&return=JSON`;
-            try {
-                const res = await app.modules["node-fetch"](postURL);
-
-
-                if (res.status != 200) {
-                    throw new Error(res.status);
-                } else {
-                    const body = await res.json();
-                    if (body["return"]["success"] == "true" && body["return"]["error"] == "none")
-                        result["succeed"].push(`${app.config.system.logURL}${body["return"]["imgUrl"]}`);
-                    else
-                        result["failed"].push(`[WEBSERVER] ${body["return"]["error"]}`);
-                };
-            } catch (err) {
-                app.logger.error("SYS", "[EVENTS] [MESSAGE DELETE] Whoops! Something went wrong! Downloading the file went OOF!\n" + err.message);
-                if (attachment.proxyURL != null) result["failed"].push(`${attachment}`);
-                else result["failed"](`[MEDIA_PROXY] ${msgAttachURL}`);
-            };
-        };
-
-        app.client.waitingForMessageToDelete = app.functions.removeItemAll(app.client.waitingForMessageToDelete, message.id);
-        return result;
-    };
-
     var channelID = serverSettings.get("loggingMessageChannel");
-
     var channel = app.client.channels.cache.get(channelID);
     if (!channel) return; // Something's wrong here?
 
     var msgAttachments;
-    if (message.attachments.size > 0)
-        msgAttachments = await downloadAttachments(message.attachments);
+    if (message.attachments.size > 0) {
+        msgAttachments = await app.functions.downloadAttachments(message, message.attachments);
 
-    while (app.client.waitingForMessageToDelete.includes(message.id)) {
-        await app.functions.sleep(1000);
+        while (app.client.waitingForMessageToDelete.includes(message.id)) {
+            await app.functions.sleep(1000);
+        };
     };
 
     embs = [{
---
+++ src/app/evts/messageDeleteBulk.js
@@ -0,0 +1,76 @@
+module.exports = async(app, messages) => {
+    const firstMsg = messages.first() || null;
+    if (!firstMsg) return;
+    else if (firstMsg.guild == null) return; // Stop if the firstMsg is empty, and if we not in a guild
+
+    var serverSettings = await app.DBs.serverSettings.findOne({ where: { serverID: messages.first().guild.id } });
+    if (!serverSettings) return;
+
+    var channelID = serverSettings.get("loggingMessageChannel");
+    var channel = app.client.channels.cache.get(channelID);
+    if (!channel) return; // Something's wrong here?
+
+    var data = new Map();
+
+    await messages.forEach(async message => {
+        if (message.partial) { await message.fetch().catch(() => messages.delete(message)); return }; // Try to get the message.
+        if (message.author.bot) { return messages.delete(message); }; // Stop if we getting data from a bot.
+
+        var msgAttachments;
+        if (message.attachments.size > 0) {
+            msgAttachments = await app.functions.downloadAttachments(message, message.attachments);
+
+            while (app.client.waitingForMessageToDelete.includes(message.id)) {
+                await app.functions.sleep(1000);
+            };
+        };
+
+        var temp = `[${message.id} | ${app.functions.convertTimestamp(message.createdTimestamp, true, true)}] ${message.author.tag}: ${message.content}`;
+        if (message.attachments.size > 0) {
+            temp = temp +
+                ((msgAttachments.succeed.length > 0) ? `\n\tAttachments: ${msgAttachments.succeed.join("\n")}` : "") +
+                ((msgAttachments.failed.length > 0) ? `\n\t**Failed (${msgAttachments.failed.length}):**\n ${msgAttachments.failed.join("\n ")}` : "");
+        };
+
+        data.set(message.id, temp);
+    });
+
+    while (data.size < messages.size) {
+        await app.functions.sleep(1000);
+    };
+
+    data = new Map([...data.entries()].sort());
+    var msgData = [];
+    data.forEach(val => msgData.push(val));
+
+    var options = {
+        embeds: [{
+            author: { name: `${messages.size} messages deleted.`, icon_url: app.client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }) },
+            color: app.config.system.embedColors.orange,
+            description: `[Start Message link](https://discord.com/channels/${messages.first().guild.id}/${messages.first().channel.id}/${messages.first().id}) | [End Message link](https://discord.com/channels/${messages.last().guild.id}/${messages.last().channel.id}/${messages.last().id})`,
+            fields: [
+                { name: "Deleted In", value: `${messages.first().channel.name} (${messages.first().channel.id}) | <#${messages.first().channel.id}>` }
+            ],
+            footer: { text: app.config.system.footerText }
+        }]
+    };
+
+    try { // WRITE TO THE FILEEE!!!
+        const fileName = `bulkDel-${firstMsg.guild.id}-${firstMsg.channel.id}_${(new Date().getTime())}.txt`;
+        const fs = app.modules["fs"],
+            fileToExportTo = `${process.cwd()}/app/cache/${fileName}`;
+
+
+        await fs.appendFile(fileToExportTo, `// Message Bulk Delete Log\n// Time deleted: ${app.functions.convertTimestamp(new Date().getTime(), true, true)}\n\n${((msgData) ? ((msgData.length > 0) ? msgData.join("\n") : "No messages fetched. Bummer.") : "Something went wrong?")}`, 'utf8', (err) => { if (err) { console.error(err); } });
+        await app.functions.sleep(1000);
+
+        channel.send(options);
+        channel.send({ files: [fileToExportTo] });
+    } catch (Ex) {
+        console.log(Ex);
+        options["embeds"][0]["fields"].unshift({ name: "Contents (First-Last)", value: ((msgData) ? ((msgData.length > 0) ? msgData.join("\n") : "No messages fetched. Bummer.") : "Something went wrong?") });
+        channel.send(options);
+    };
+
+
+};
