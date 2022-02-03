45cfd4d491b983400a96ed128061e31a77ee689f
IDeletedSystem64 <anthonymondz@gmail.com>
2022-02-02 21:48:22 -0600

    Change cooldown, set to guildOnly. Fix succ. message

--- src/app/cmds/Moderation/purge.js
+++ src/app/cmds/Moderation/purge.js
@@ -1,14 +1,13 @@
 module.exports = {
     name: "purge",
     description: "Purge those spicy messages",
-    category: "Moderation",
-    guildOnly: false,
+    guildOnly: true,
     authorizedGuilds: [],
     hidden: false,
     permissions: ["MANAGE_MESSAGES"],
-    cooldown: 2,
+    cooldown: 5,
     aliases: ["prune"],
-    syntax: [" <Message Count>"],
+    syntax: [" <Count>"],
     execute: async(app, message, args) => {
         let msgcount = args.slice(" ")
         
@@ -23,13 +22,20 @@ module.exports = {
                                 title: `${app.config.system.emotes.success} Purge Successful!`,
                                 color: app.config.system.embedColors.lime,
                                 description: `Successfully purged ${prunedMsgs.size} spicy messages!`,
-                                footer: { text: app.config.system.footerText }
+                                footer: { text: app.config.system.footerText + " | This message will blow up in 5 seconds."}
                             }]
+                            }, 0, false, (msg => { setTimeout(() => msg.delete(), 5000) }));
                             });
-                });
             });
                 } catch (e) {
-                    
+                    app.functions.msgHandler(message, {
+                        embeds: [{
+                            title: `${app.config.system.emotes.error} Uh-Oh! Something went wrong!`,
+                            color: app.config.system.embedColors.red,
+                            description: e.message
+                            footer: { text: app.config.system.footerText + " | An error occurred."}
+                        }]
+                    });
             }
         } else {
             app.functions.msgHandler(message, {
