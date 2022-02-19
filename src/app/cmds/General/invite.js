076095a8035efeb810a0ad0253ab775bb1d66006
IDeletedSystem64 <anthonymondz@gmail.com>
2022-02-18 21:50:11 -0600

    Add that invite cmd so peeps can add to them fine fine servers

---
+++ src/app/cmds/General/invite.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "invite",
+    description: "Add me to yo server!",
+    guildOnly: false,
+    authorizedGuilds: [],
+    hidden: false,
+    permissions: ["DEFAULT"],
+    cooldown: 2,
+    aliases: [],
+    syntax: [],
+    execute: async(app, message, args) => {
+        return app.functions.msgHandler(message, {
+        embeds: [{
+            title: app.config.system.emotes.information + " Invite",
+            color: app.config.system.embedColors.green,
+            fields: [
+                { name: "OwO? New server?!?", value: `Aw heck yeah!! Add me and become one of the ${app.client.guilds.cache.size} servers using TheCodingBot!`},
+                { name: "Add me to dat server!", value: "Add me via [Discord](https://discord.com/oauth2/authorize?scope=bot&client_id=438532019924893707&permissions=8)"},
+                { name: "...is something wrong..?", value: "Join the [TMC Discord](https://discord.gg/HdKeWtV) or message <@222073294419918848>!" },
+        ]}]
+    })
+}}
