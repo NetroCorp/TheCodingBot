8c7a2afee27f26b6bbd30903878960d54ac4a52a
wrac4242 <wrac4242@gmail.com>
2022-03-18 19:10:42 +0000

    Add source command

---
+++ src/app/cmds/General/source.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "source",
+    description: "A link to the source for the bot!",
+    guildOnly: false,
+    authorizedGuilds: [],
+    hidden: false,
+    permissions: ["DEFAULT"],
+    cooldown: 2,
+    aliases: [],
+    syntax: [],
+    execute: async(app, message, args) => {
+        return app.functions.msgHandler(message, {
+        embeds: [{
+            title: app.config.system.emotes.information + " Source",
+            color: app.config.system.embedColors.green,
+            fields: [
+                { name: "OwO? Source Code?!?", value: `The source code for TheCodingBot can be found [here](https://tcb.nekos.tech/)!` }
+        ]}]
+    })
+}}
